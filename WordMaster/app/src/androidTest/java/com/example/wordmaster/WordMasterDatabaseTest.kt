a8c94c865a2a739a0021ae0797fe8c636917b9c0
pathways35 <safeena.hameed@gmail.com>
2019-08-16 20:18:58 -0400

    Deleted GameWord Entity and Dao

--- WordMaster/app/src/androidTest/java/com/example/wordmaster/WordMasterDatabaseTest.kt
+++ WordMaster/app/src/androidTest/java/com/example/wordmaster/WordMasterDatabaseTest.kt
@@ -3,22 +3,17 @@ package com.example.wordmaster
 import androidx.room.Room
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-import com.example.wordmaster.database.GameWordDao
-import com.example.wordmaster.database.Model.Word
 import com.example.wordmaster.database.WordDatabaseDao
 import com.example.wordmaster.database.WordMasterDatabase
 import org.junit.After
 import org.junit.Before
-import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
-import java.lang.Exception
 
 @RunWith(AndroidJUnit4::class)
 class WordMasterDatabaseTest {
 
     private lateinit var wordDao: WordDatabaseDao
-    private lateinit var gameWordDao: GameWordDao
 
     private lateinit var db: WordMasterDatabase
 
@@ -31,7 +26,6 @@ class WordMasterDatabaseTest {
             .build()
 
         wordDao = db.wordDatabaseDao
-        gameWordDao = db.gameWordDao
     }
 
     @After
